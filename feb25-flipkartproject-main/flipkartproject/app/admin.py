45bf071e5934b4b567b6a33055ce2c4bbc7ae21a
kinjaltailor <83589855+kinjaltailor@users.noreply.github.com>
2025-03-05 11:32:00 +0530

    Add files via upload

---
+++ feb25-flipkartproject-main/flipkartproject/app/admin.py
@@ -0,0 +1,57 @@
+from django.contrib import admin
+from .models import Product, Cart, Orders,Address,Payment
+
+# Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "userid",
+        "productid",
+        "productname",
+        "category",
+        "description",
+        "price",
+        "images",
+    ]
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ["userid", "productid", "qty"]
+
+
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = [
+        "orderid",
+        "userid",
+        "productid",
+        "qty",
+    ]
+
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display=[
+        "userid",
+        "mobile",
+        "address",
+        "pincode"
+    ]
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display=[
+        "receiptid",
+        "orderid",
+        "userid",
+        "productid",
+        "totalprice"
+    ]
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Cart, CartAdmin)
+admin.site.register(Orders, OrdersAdmin)
+admin.site.register(Address,AddressAdmin)
+admin.site.register(Payment,PaymentAdmin)
+
+
+# Register your models here.
---
+++ feb25-flipkartproject-main/flipkartproject/app/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("signin/", views.signin, name="signin"),
+    path("signup/", views.signup, name="signup"),
+    path("userlogout/", views.userlogout, name="userlogout"),
+    path(
+        "request_password_reset/",
+        views.request_password_reset,
+        name="request_password_reset",
+    ),
+    path("reset_password/<uname>/", views.reset_password, name="reset_password"),
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact'),
+    path('mobilelist/',views.mobilelist,name='mobilelist'),
+    path('clothlist/',views.clothlist,name='clothlist'),
+    path('shoeslist/',views.shoeslist,name='shoeslist'),
+    path('electronicslist/',views.electronicslist,name='electronicslist'),
+    path('showpricerange/',views.showpricerange,name='showpricerange'),
+    path('sortingbyprice/',views.sortingbyprice,name='sortingbyprice'),
+    path('searchproduct/',views.searchproduct,name='searchproduct'),
+    path('showcarts/',views.showcarts,name='showcarts'),
+    path('addtocart/<productid>/',views.addtocart,name='addtocart'),
+    path('removecart/<productid>/',views.removecart,name='removecart'),
+    path('updateqty/<int:qv>/<productid>',views.updateqty,name='updateqty'),
+    path('addaddress/',views.addaddress,name='addaddress'),
+    path('showaddress/',views.showaddress,name='showaddress'),
+    path('payment/',views.payment,name='payment')
+]
